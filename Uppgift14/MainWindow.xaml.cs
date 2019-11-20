5f2fe073f2e139a9dc1328a929207843d2e09d60
EvelinaNilsson <57352562+EvelinaNilsson@users.noreply.github.com>
2019-11-20 12:44:01 +0100

    Uppgift 14 och en bit på vägen med uppgift 15

---
+++ Uppgift14/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift14
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public bool IsLetter()
+        {
+            string yourBirthYear = txtBirthYear.Text;
+
+            foreach (char c in yourBirthYear)
+            {
+                if (Char.IsLetter(c))
+                {
+                    MessageBox.Show("Du får enbart mata in siffror!");
+
+                    return true;
+                    
+                    //Får inte fortsätta och rätta till. Hur gör jag för att få komma tillbaka till txtBirthYear?
+
+                }
+
+            }
+
+            return false;
+        }
+
+        private void btnCalcAge_Click(object sender, RoutedEventArgs e)
+        {
+            bool result = IsLetter();
+
+            int birthYear = int.Parse(txtBirthYear.Text);
+            int yourAge = DateTime.Now.Year - birthYear;
+
+            MessageBox.Show($"Du är {yourAge} år gammal.");
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Uppgift15/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift15
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            string yourMessage = txtMessage.Text;
+        }
+        
+
+        public bool IsVowel(char isVowel)
+        {
+
+            char[] vowelsArray = new char[] { 'a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', 'ö', 'A', 'E', 'I', 'O', 'U', 'Y', 'Å', 'Ä', 'Ö' };
+            //mitt facit till vokaler
+            bool vowel = false;
+
+            foreach (char c in vowelsArray)
+            {
+                if ((c) == isVowel)
+                {
+                    vowel = true;
+                }
+            }
+
+            return vowel;
+        }
+
+        public int NumberOfVowels(string numberOfVowels) //Varför har jag en sträng här???
+        {
+            int count = 0;
+
+            foreach (char c in numberOfVowels)
+            {
+                if (IsVowel(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public string Jibberish(char convertTo)
+        {
+            foreach (char c in collection)
+            {
+
+            }
+           
+           
+        }
+        
+
+
+        //public bool IsNumberOfVowels()  //räkna vokalerna
+        //{
+        //    int numberOfVowels = 0;
+
+        //    foreach (char c in yourMessage)
+        //    {
+        //        bool isVowel = "aeiouyåäöAEIOUYÅÄÖ".IndexOf(c) >= 0; //kollar om vokaler (= a, e, i, o, u, y, å, ä, ö)
+
+        //        if (true)
+        //        {
+        //            numberOfVowels++;
+        //        }
+        //    }
+        //    return numberOfVowels;
+
+    }
+
+}
